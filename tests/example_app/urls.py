1f7e14788396ce046f39d0457b755df203dbdc72
Iacopo Spalletti <i.spalletti@nephila.it>
2023-04-18 22:37:10 +0200

    Update test urlconf

--- tests/example_app/urls.py
+++ tests/example_app/urls.py
@@ -1,26 +1,18 @@
-import django.views.i18n
-import django.views.static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
+from django.views.i18n import JavaScriptCatalog
+from django.views.static import serve
 
 from .views import PostDetailView, PostListView, PostMixinDetailView
 
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(
-        r"^media/(?P<path>.*)$",
-        django.views.static.serve,  # NOQA
-        {"document_root": settings.MEDIA_ROOT, "show_indexes": True},
-    ),
-    re_path(r"^admin/", admin.site.urls),  # NOQA
-    re_path(r"^jsi18n/(?P<packages>\S+?)/$", django.views.i18n.JavaScriptCatalog.as_view()),  # NOQA
-    re_path(
-        r"^mixin/(?P<slug>\w[-\w]*)/$",
-        PostMixinDetailView.as_view(),
-        name="post-detail-mixinx",
-    ),
-    re_path(r"^(?P<slug>\w[-\w]*)/$", PostDetailView.as_view(), name="post-detail"),
+    path("admin/", admin.site.urls),
+    path("media/<str:path>", serve, {"document_root": settings.MEDIA_ROOT, "show_indexes": True}),
+    path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
+    path("mixin/<slug:slug>/", PostMixinDetailView.as_view(), name="post-detail-mixinx"),
+    path("<slug:slug>/", PostDetailView.as_view(), name="post-detail"),
     path("", PostListView.as_view(), name="post-list"),
 ]
